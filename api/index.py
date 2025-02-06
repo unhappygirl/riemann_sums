2a0eddb5e1709cbd251651536381738a8ad69f1c
teknosa <aydin.turanli@sabanciuniv.edu>
2025-02-06 21:31:27 +0300

    commit all

--- api/index.py
+++ api/index.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("static/node_modules/mathquill/build/mathquill.css")
+def evaluatex():
+    return render_template("index.html")
+@app.route("static/node_modules/mathquill/build/mathquill.css")
+def mathquill():
+    return render_template("index.html")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/favicon.ico")
 def icon():
     return open("./favicon.ico", "rb").read()
